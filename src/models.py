442ca8518290050993af45f03e222f31b3bef29a
BilelMatallah <bilelmatallah@hotmail.com>
2023-03-25 12:22:26 +0000

    feat: proy finalizado

--- src/models.py
+++ src/models.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table user
+    # Here we define columns for the table user.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     username = Column(String(250), nullable=False)
@@ -21,7 +21,6 @@ class User(Base):
 class Planet(Base):
     __tablename__ = 'planet'
     # Here we define columns for the table planet.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     orbit = Column(String(250))
@@ -30,21 +29,18 @@ class Planet(Base):
 class Vehicle(Base):
     __tablename__ = 'vehicle'
     # Here we define columns for the table vehicle.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Character(Base):
     __tablename__ = 'character'
     # Here we define columns for the table character.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Favorites(Base):
     __tablename__ = 'favorites'
     # Here we define columns for the table favorites.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -58,7 +54,7 @@ class Favorites(Base):
 
     character_id = Column(Integer, ForeignKey('character.id'))
     character = relationship(Character)
-    
+
     def to_dict(self):
         return {}
 
